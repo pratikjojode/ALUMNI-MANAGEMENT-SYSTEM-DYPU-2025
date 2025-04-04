a81b3af7603b03de17091623bbb70ac0b7661f67
pratikjojode <139045460+pratikjojode@users.noreply.github.com>
2025-04-04 17:08:01 +0530

    new

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import "./index.css";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello Alumni! 🎓</h1>
+    </>
+  );
+};
+
+export default App;
